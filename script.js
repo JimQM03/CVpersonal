16bbfd7ee59ab78f37503efa446642b32d0e03d1
JimQM03 <jimq293@gmail.com>
2026-02-03 15:13:36 -0500

    Mejora de visuales

--- script.js
+++ script.js
@@ -14,21 +14,47 @@ const cuerpoPagina = document.body; // Referencia directa al <body> del HTML
 // Ahora usamos un Array de OBJETOS para almacenar múltiples propiedades (nombre y clase de ícono)
 const habilidadesLista = [
     { nombre: "JavaScript", 
-        // 'fa-brands fa-js' es la clase de Font Awesome para el ícono de JavaScript
-        claseIcono: "fa-brands fa-js"  },
+        
+      claseIcono: "fa-brands fa-js",// 'fa-brands fa-js' es la clase de Font Awesome para el ícono de JavaScript
+
+      color: "#f7df1e" // Amarillo JS
+    
+    },
     { nombre: "Python", 
-        // 'fa-brands fa-python' es la clase de Font Awesome para el ícono de Python
-        claseIcono: "fa-brands fa-python" },
-    { nombre: "HTML", 
-        // 'fa-brands fa-html5' es la clase de Font Awesome para el ícono de HTML
-        claseIcono: "fa-brands fa-html5" },
+        
+      claseIcono: "fa-brands fa-python", // 'fa-brands fa-python' es la clase de Font Awesome para el ícono de Python
+
+      color: "#3776ab"
+    
+    },
+    { nombre: "HTML",
+        
+      claseIcono: "fa-brands fa-html5", // 'fa-brands fa-html5' es la clase de Font Awesome para el ícono de HTML
+
+      color: "#e34f26"
+    
+    },
     { nombre: "MySQL", 
-        // 'fa-solid fa-database' es la clase de Font Awesome para un ícono de base de datos genérico
-     claseIcono: "fa-solid fa-database"  },
+        
+     claseIcono: "fa-solid fa-database", // 'fa-solid fa-database' es la clase de Font Awesome para un ícono de base de datos genérico
+
+     color: "#00758f"
+    
+    },
     { nombre: "Github",
-        claseIcono: "fab fa-github"      },
-    { nombre: "Git",
-        claseIcono: "fab fa-git"         }
+
+      claseIcono: "fab fa-github", // 'fab fa-github' es la clase de Font Awesome para un ícono de base de github  
+    
+      color: "#6e5494"
+
+    },
+    { nombre: "Git",   
+
+      claseIcono: "fab fa-git",    // 'fab fa-git' es la clase de Font Awesome para un ícono de Git   
+    
+      color: "#f05032"
+
+    }
 ];
 
 //Elemento de captura del boton de traducción
@@ -70,6 +96,7 @@ function insertarHabilidades() {
             const icono = document.createElement('i');
             //  Asignar la clase de Font Awesome y el nombre
             icono.className = habilidad.claseIcono; // Ejemplo: "fa-brands fa-js"
+            itemLista.style.setProperty('--color-hover', habilidad.color);
             // Insertar el ícono dentro del <li>
             itemLista.appendChild(icono);
             //Adjuntar el <li> a la LISTA ACTUAL (tanto la de español como la de inglés)
